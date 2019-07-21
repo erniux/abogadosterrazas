dd2a6172560169a3486647bc0e6279e064be1591
erniux <erna.tercero@hotmail.com>
2019-07-20 19:09:43 -0500

    actualiacion de locales

--- app/models/audiencia_expediente.rb
+++ app/models/audiencia_expediente.rb
@@ -1,3 +1,14 @@
 class AudienciaExpediente < ApplicationRecord
   belongs_to :expediente
+
+  before_save :to_upper
+
+
+  private
+  def to_upper
+    attributes.keys.each do |attribute|
+      self[attribute].try(:upcase!)
+    end
+  end
+  
 end
