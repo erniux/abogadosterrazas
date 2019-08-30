a5c4088584b8461b36ce9bb539ccdf93fff3a04b
erniux <erna.tercero@hotmail.com>
2019-08-30 07:57:48 -0500

    mas mejoras responsivas

--- app/controllers/paginas_controller.rb
+++ app/controllers/paginas_controller.rb
@@ -1,6 +1,6 @@
 class PaginasController < ApplicationController
   def inicio
-  	@inicio_semana = Date.yesterday
+  	@inicio_semana = Date.today
   	@fin_semana = @inicio_semana + 7.days
   	@audiencias = AudienciaExpediente.where('fecha >= ? and fecha < ?', @inicio_semana, @fin_semana).order("fecha ASC").page(params[:page]) 
   end
