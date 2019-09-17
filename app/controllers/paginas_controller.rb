941d9f632d97c1f63f1ad99c2c3c019dbd16a897
erniux <erna.tercero@hotmail.com>
2019-09-17 15:11:51 -0500

    ordenando los resultados de las consultas

--- app/controllers/paginas_controller.rb
+++ app/controllers/paginas_controller.rb
@@ -7,7 +7,8 @@ class PaginasController < ApplicationController
   		@audiencias = AudienciaExpediente.find_by_sql("SELECT * FROM audiencia_expedientes a, expedientes e 
   										  WHERE a.fecha > '#{@inicio_semana}' and a.fecha < '#{@fin_semana}' 
   										  AND a.expediente_id = e.id 
-  										  AND e.entidad_responsable_id = #{params[:entidad_responsable_id]}") 
+  										  AND e.entidad_responsable_id = #{params[:entidad_responsable_id]}
+  										  ORDER BY a.fecha ASC") 
   	else
   		@audiencias = AudienciaExpediente.where('fecha >= ? and fecha < ?', @inicio_semana, @fin_semana)
   										 .order("fecha ASC") 
