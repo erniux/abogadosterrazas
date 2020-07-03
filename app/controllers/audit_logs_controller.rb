bb9ddaa9c6101643b766133b16868a91bb5acc51
erniux <erna.tercero@hotmail.com>
2020-07-02 23:43:39 -0500

    mejoras 20200702

--- app/controllers/audit_logs_controller.rb
+++ app/controllers/audit_logs_controller.rb
@@ -2,13 +2,11 @@ class AuditLogsController < ApplicationController
   	access  admin: :all
 
 	def index
-    #@expedientes = Expediente.all.order("anio DESC")
-    if params[:term]
-      @bitacora = AuditLog.search_by_full_auditlog(params[:term]).order(created_at: 'DESC')
-    elsif params[:term].blank?
-      @bitacora = AuditLog.all.order(created_at: 'DESC', expediente: 'DESC') 
-    end 
-
-  end
-
+    	if params[:term]
+      		@bitacora = AuditLog.search_by_full_auditlog(params[:term]).order(created_at: 'DESC').page(params[:page])
+    	elsif params[:term].blank?
+      		@bitacora = AuditLog.all.order(created_at: 'DESC', expediente: 'DESC').page(params[:page])  
+    	end 
+  	end
+  	
 end
