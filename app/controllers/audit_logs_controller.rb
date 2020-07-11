7bc24755a576f442494ec4d946fd107929ad83a3
erniux <erna.tercero@hotmail.com>
2020-07-10 19:41:52 -0500

    Agregar track a usuarios

--- app/controllers/audit_logs_controller.rb
+++ app/controllers/audit_logs_controller.rb
@@ -5,7 +5,7 @@ class AuditLogsController < ApplicationController
     	if params[:term]
       		@bitacora = AuditLog.search_by_full_auditlog(params[:term]).order(created_at: 'DESC').page(params[:page])
     	elsif params[:term].blank?
-      		@bitacora = AuditLog.all.order(created_at: 'DESC', expediente: 'DESC').page(params[:page])  
+      		@bitacora = AuditLog.where("Expediente IS NOT NULL").order(created_at: 'DESC', expediente: 'DESC').page(params[:page])  
     	end 
     	AuditLog.create!(current_sign_in_ip: current_user.current_sign_in_ip,  user_id: current_user.id, accion: 'Menú audit logs' )
   	end
