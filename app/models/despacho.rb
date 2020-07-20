164c301be3b2e7967e954994693d74d2a61b4952
erniux <erna.tercero@hotmail.com>
2020-07-19 22:17:21 -0500

    bug corregido de Locations

--- app/models/despacho.rb
+++ app/models/despacho.rb
@@ -1,6 +1,6 @@
 class Despacho < ApplicationRecord
 	has_many :locations, inverse_of: :despacho, dependent: :destroy 
-	belongs_to :abogados
+	 
 
 	accepts_nested_attributes_for :locations, reject_if: :all_blank, allow_destroy: true
 
